50e34ba6639815e280f0a6b3ac446dfd4ce107d6
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2023-12-03 19:41:03 +0600

    folder struture issue solved

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,24 @@
+<div>
+    <div>
+        <div>
+            <h1>Luna Bee</h1>
+        </div>
+        <div>
+            <li>
+                Home
+            </li>
+            <li>
+                Shop
+            </li>
+            <li>
+                Blog
+            </li>
+            <li>
+                Contact
+            </li>
+        </div>
+        <div>
+
+        </div>
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Home.home');
 });
 
 Route::get('/dashboard', function () {
