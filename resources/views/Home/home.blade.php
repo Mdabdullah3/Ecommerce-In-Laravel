91af37426df630ba4f788b213d5ede1382232346
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2023-12-03 20:54:57 +0600

    navbar done

--- resources/views/Home/home.blade.php
+++ resources/views/Home/home.blade.php
@@ -4,19 +4,24 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <title>LunaBee</title>
-
+    <!-- Font Awesome  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <link href="https://fonts.gstatic.com/s/heebo/v22/NGS6v5_NC0k9P9H2TbE.woff2) format('woff2');
+  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD" rel="stylesheet" />
+    <!-- Tailwind Css  -->
     <script src="https://cdn.tailwindcss.com"></script>
+    <style>
+        body {
+            font-family: 'Heebo';
+        }
+    </style>
 </head>
 
 <body class="">
     @include('layouts.navbar')
     @include('Home.Components.Banner')
-    @include('Home.Components.about')
 
     <!-- @if (Route::has('login'))
     <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,14 +1,32 @@
-<div>
-    <div>
+<div class="w-10/12 mx-auto my-10">
+    <div class="flex justify-between">
         <div>
-            <h1>Luna Bee</h1>
+            <h1 class="text-3xl font-semibold tracking-widest">Luna Bee.</h1>
         </div>
-        <div>
+        <div class=" list-none flex gap-10 tracking-widest text-lg">
             <li>
                 Home
             </li>
-            <li>
-                Shop
+            <li class="relative group">
+                <span class="cursor-pointer">
+                    Shop
+                </span>
+                <div class="absolute w-96 left-0 mt-2 bg-white border rounded-lg p-4 space-y-2 hidden group-hover:block transition duration-300">
+                    <div class="grid grid-cols-4 gap-4 text-sm tracking-widest text-gray-500 cursor-pointer">
+                        <div class="text-black">Men</div>
+                        <div class="text-black">Women</div>
+                        <div class="text-black">Kids</div>
+                        <div>Pants</div>
+                        <div>T-Shirts</div>
+                        <div>Accessories</div>
+                        <div>Shirts</div>
+                        <div class="text-black">Watch</div>
+                        <div>Panjabi</div>
+                        <div>Bags</div>
+                        <div>Shoes</div>
+                        <div>Rings</div>
+                    </div>
+                </div>
             </li>
             <li>
                 Blog
@@ -17,8 +35,10 @@
                 Contact
             </li>
         </div>
-        <div>
-
+        <div class="flex gap-10 tracking-widest text-lg">
+            <h1 class="text-black"><i class="fa-solid fa-magnifying-glass"></i></h1>
+            <h1 class="text-black"><i class="fa-solid fa-cart-shopping"></i></h1>
+            <h1><i class="fa-solid fa-user"></i></h1>
         </div>
     </div>
 </div>
