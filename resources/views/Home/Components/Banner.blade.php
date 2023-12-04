e6e80e755d0eaf6d4bb30777bec5db34160c428d
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2023-12-04 12:24:22 +0600

    banner section design

--- resources/views/Home/Components/Banner.blade.php
+++ resources/views/Home/Components/Banner.blade.php
@@ -1,15 +1,10 @@
 <!-- Swiper -->
-<div class="swiper mySwiper">
+<div class="swiper mySwiper -z-10">
     <div class="swiper-wrapper">
-        <div class="swiper-slide">Slide 1</div>
-        <div class="swiper-slide">Slide 2</div>
-        <div class="swiper-slide">Slide 3</div>
-        <div class="swiper-slide">Slide 4</div>
-        <div class="swiper-slide">Slide 5</div>
-        <div class="swiper-slide">Slide 6</div>
-        <div class="swiper-slide">Slide 7</div>
-        <div class="swiper-slide">Slide 8</div>
-        <div class="swiper-slide">Slide 9</div>
+        <div class="swiper-slide"><img src="https://minimog-4437.kxcdn.com/next/wp-content/uploads/sites/6/2022/08/soap-hero-bg.jpg" alt=""></div>
+        <div class="swiper-slide"><img src="https://minimog.thememove.com/wp-content/uploads/2021/12/h1_slide_01.jpg" alt=""></div>
+        <div class="swiper-slide"><img src="https://minimog.thememove.com/wp-content/uploads/2022/02/h_juice_slide_01.jpg" alt=""></div>
+        <div class="swiper-slide"><img src="https://minimog.thememove.com/wp-content/uploads/2021/12/h4_slide_01.jpg" alt=""></div>
     </div>
     <div class="swiper-button-next"></div>
     <div class="swiper-button-prev"></div>
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,44 +1,49 @@
-<div class="w-10/12 mx-auto my-10">
-    <div class="flex justify-between">
-        <div>
-            <h1 class="text-3xl font-semibold tracking-widest">Luna Bee.</h1>
-        </div>
-        <div class=" list-none flex gap-10 tracking-widest text-lg">
-            <li>
-                Home
-            </li>
-            <li class="relative group">
-                <span class="cursor-pointer">
-                    Shop
-                </span>
-                <div class="absolute w-96 left-0 mt-2 bg-white border rounded-lg p-4 space-y-2 hidden group-hover:block transition duration-300">
-                    <div class="grid grid-cols-4 gap-4 text-sm tracking-widest text-gray-500 cursor-pointer">
-                        <div class="text-black">Men</div>
-                        <div class="text-black">Women</div>
-                        <div class="text-black">Kids</div>
-                        <div>Pants</div>
-                        <div>T-Shirts</div>
-                        <div>Accessories</div>
-                        <div>Shirts</div>
-                        <div class="text-black">Watch</div>
-                        <div>Panjabi</div>
-                        <div>Bags</div>
-                        <div>Shoes</div>
-                        <div>Rings</div>
+<div>
+    <div class="bg-[#DA3F3F] py-2">
+        <h1 class="text-md tracking-wider capitalize text-center text-white">Free Delivery on orders over $200. Don’t miss discount.</h1>
+    </div>
+    <div class="w-10/12 mx-auto my-6">
+        <div class="flex justify-between items-center">
+            <div>
+                <h1 class="text-3xl font-semibold tracking-widest">Luna Bee.</h1>
+            </div>
+            <div class=" list-none flex gap-10 tracking-widest text-lg items-center">
+                <li>
+                    Home
+                </li>
+                <li class="relative group">
+                    <span class="cursor-pointer">
+                        Shop
+                    </span>
+                    <div class="absolute w-96 left-0 top-5 mt-2 bg-gray-900 border rounded-lg p-4 space-y-2 hidden group-hover:block transition duration-300">
+                        <div class="grid grid-cols-4 gap-4 text-sm tracking-widest text-gray-400 py-4 cursor-pointer">
+                            <div class="text-white">Men</div>
+                            <div class="text-white-500">Women</div>
+                            <div class="text-white">Kids</div>
+                            <div>Pants</div>
+                            <div>T-Shirts</div>
+                            <div>Accessories</div>
+                            <div>Shirts</div>
+                            <div class="text-white">Watch</div>
+                            <div>Panjabi</div>
+                            <div>Bags</div>
+                            <div>Shoes</div>
+                            <div>Rings</div>
+                        </div>
                     </div>
-                </div>
-            </li>
-            <li>
-                Blog
-            </li>
-            <li>
-                Contact
-            </li>
-        </div>
-        <div class="flex gap-10 tracking-widest text-lg">
-            <h1 class="text-black"><i class="fa-solid fa-magnifying-glass"></i></h1>
-            <h1 class="text-black"><i class="fa-solid fa-cart-shopping"></i></h1>
-            <h1><i class="fa-solid fa-user"></i></h1>
+                </li>
+                <li>
+                    Blog
+                </li>
+                <li>
+                    Contact
+                </li>
+            </div>
+            <div class="flex gap-10 tracking-widest text-lg">
+                <h1 class="text-black"><i class="fa-solid fa-magnifying-glass"></i></h1>
+                <h1 class="text-black"><i class="fa-solid fa-cart-shopping"></i></h1>
+                <h1><i class="fa-solid fa-user"></i></h1>
+            </div>
         </div>
     </div>
 </div>
