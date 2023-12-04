f94fcef1835ea732673bd0984af9ab2e8c252f6e
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2023-12-04 12:55:53 +0600

    swiper js customize

--- resources/views/Home/Components/Banner.blade.php
+++ resources/views/Home/Components/Banner.blade.php
@@ -1,6 +1,6 @@
 <!-- Swiper -->
 <div class="swiper mySwiper -z-10">
-    <div class="swiper-wrapper">
+    <div class="swiper-wrapper h-screen">
         <div class="swiper-slide"><img src="https://minimog-4437.kxcdn.com/next/wp-content/uploads/sites/6/2022/08/soap-hero-bg.jpg" alt=""></div>
         <div class="swiper-slide"><img src="https://minimog.thememove.com/wp-content/uploads/2021/12/h1_slide_01.jpg" alt=""></div>
         <div class="swiper-slide"><img src="https://minimog.thememove.com/wp-content/uploads/2022/02/h_juice_slide_01.jpg" alt=""></div>
@@ -8,5 +8,5 @@
     </div>
     <div class="swiper-button-next"></div>
     <div class="swiper-button-prev"></div>
-    <div class="swiper-pagination"></div>
+    <div class="swiper-pagination -mt-20"></div>
 </div>
--- resources/views/Home/home.blade.php
+++ resources/views/Home/home.blade.php
@@ -10,12 +10,20 @@
     <!-- Fonts -->
     <link href="https://fonts.gstatic.com/s/heebo/v22/NGS6v5_NC0k9P9H2TbE.woff2) format('woff2');
   unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD" rel="stylesheet" />
+    <!-- Swiper js  -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
     <!-- Tailwind Css  -->
     <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+
+
     <style>
         body {
             font-family: 'Heebo';
+        }
+
+        :root {
+            --swiper-navigation-size: 20px;
+            --swiper-theme-color: #977f7f;
         }
 
         .swiper {
@@ -37,6 +45,53 @@
             width: 100%;
             height: 100%;
             object-fit: cover;
+        }
+
+        .swiperbtn .swiper-wrapper .swiper-slide-active .swiper-button-next {
+            position: absolute;
+            right: 18%;
+            top: 0%;
+            border: 1px solid #977f7f;
+            padding: 30px;
+            border-radius: 100%;
+        }
+
+        .swiper-button-prev {
+            position: absolute;
+            left: 5%;
+            top: 45%;
+            border: 1px solid #977f7f;
+            padding: 30px;
+            border-radius: 100%;
+        }
+
+        .swiper-button-next {
+            position: absolute;
+            right: 5%;
+            top: 45%;
+            border: 1px solid #977f7f;
+            padding: 30px;
+            border-radius: 100%;
+        }
+
+        .swiper-pagination-bullet {
+            width: var(--swiper-pagination-bullet-width,
+                    var(--swiper-pagination-bullet-size, 8px));
+            height: var(--swiper-pagination-bullet-height,
+                    var(--swiper-pagination-bullet-size, 8px));
+            display: inline-block;
+            background: #5d5a5a;
+            border: 10px;
+            opacity: var(--swiper-pagination-bullet-inactive-opacity, 0.2);
+        }
+
+        .swiper-pagination-bullet-active {
+            width: var(--swiper-pagination-bullet-width,
+                    var(--swiper-pagination-bullet-size, 12px));
+            height: var(--swiper-pagination-bullet-height,
+                    var(--swiper-pagination-bullet-size, 12px));
+            opacity: var(--swiper-pagination-bullet-opacity, 1);
+            background: var(--swiper-pagination-color, var(--swiper-theme-color));
         }
     </style>
 </head>
