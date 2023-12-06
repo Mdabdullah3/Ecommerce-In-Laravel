b1a11af20517415a88d991c026f4c9dbc61286b6
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2023-12-06 12:13:33 +0600

    product details design done

--- resources/views/Products/ProductDetails.blade.php
+++ resources/views/Products/ProductDetails.blade.php
@@ -27,7 +27,7 @@ unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+
 <body>
     @include('layouts.navbar')
     <section class="overflow-hidden py-11">
-        <div class="max-w-6xl px-4 py-4 mx-auto lg:py-8 md:px-6">
+        <div class="max-w-7xl px-4 py-4 mx-auto lg:py-8 md:px-6">
             <div class="flex flex-wrap -mx-4">
                 <div class="w-full px-4 md:w-1/2 ">
                     <div class="sticky top-0 z-50 overflow-hidden ">
@@ -66,139 +66,110 @@ unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+
                 <div class="w-full px-4 md:w-1/2 ">
                     <div class="lg:pl-20">
                         <div class="mb-8 ">
-                            <span class="text-lg font-medium text-rose-500 dark:text-rose-200">New</span>
-                            <h2 class="max-w-xl mt-2 mb-6 text-2xl font-bold dark:text-gray-400 md:text-4xl">
-                                Shoes</h2>
-                            <div class="flex items-center mb-6">
-                                <ul class="flex mr-2">
-                                    <li>
-                                        <a href="#">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                                fill="currentColor"
-                                                class="w-4 mr-1 text-red-500 dark:text-gray-400 bi bi-star "
-                                                viewBox="0 0 16 16">
-                                                <path
-                                                    d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
-                                            </svg>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                                fill="currentColor"
-                                                class="w-4 mr-1 text-red-500 dark:text-gray-400 bi bi-star "
-                                                viewBox="0 0 16 16">
-                                                <path
-                                                    d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
-                                            </svg>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                                fill="currentColor"
-                                                class="w-4 mr-1 text-red-500 dark:text-gray-400 bi bi-star "
-                                                viewBox="0 0 16 16">
-                                                <path
-                                                    d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
-                                            </svg>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                                fill="currentColor"
-                                                class="w-4 mr-1 text-red-500 dark:text-gray-400 bi bi-star "
-                                                viewBox="0 0 16 16">
-                                                <path
-                                                    d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
-                                            </svg>
-                                        </a>
-                                    </li>
-                                </ul>
-                                <p class="text-xs dark:text-gray-400 ">(2 customer reviews)</p>
-                            </div>
-                            <p class="max-w-md mb-8 text-gray-700 dark:text-gray-400">
-                                Lorem ispum dor amet Lorem ispum dor amet Lorem ispum dor amet Lorem ispum dor amet
-                                Lorem ispum dor amet Lorem ispum dor amet Lorem ispum dor amet Lorem ispum dor amet
+                            <h2
+                                class="text-md tracking-widest bg-red-500 text-white font-semibold px-3 py-2 w-16 rounded-full my-3">
+                                -10%</h2>
+                            <h1 class="text-3xl font-semibold tracking-widest mb-3">Trainers Sneakers</h1>
+                            <p class="inline-block mb-6 tracking-wider text-3xl text-black">
+                                <span>$100.99</span>
+                                <span class="text-base font-normal text-gray-500 line-through ">$1500.99</span>
                             </p>
-                            <p class="inline-block mb-8 text-4xl font-bold text-gray-700 dark:text-gray-400 ">
-                                <span>$1000.99</span>
-                                <span
-                                    class="text-base font-normal text-gray-500 line-through dark:text-gray-400">$1500.99</span>
-                            </p>
-                            <p class="text-green-600 dark:text-green-300 ">7 in stock</p>
+                            <h1 class="text-sm tracking-widest"><span><i class="fa-regular fa-eye"></i></span>
+                                <span class="font-bold">21</span> people
+                                are viewing this right now
+                            </h1>
                         </div>
                         <div class="flex items-center mb-8">
-                            <h2 class="w-16 mr-6 text-xl font-bold dark:text-gray-400">
-                                Colors:</h2>
-                            <div class="flex flex-wrap -mx-2 -mb-2">
-                                <button
-                                    class="p-1 mb-2 mr-2 border border-transparent hover:border-blue-400 dark:border-gray-800 dark:hover:border-gray-400 ">
-                                    <div class="w-6 h-6 bg-cyan-300"></div>
-                                </button>
-                                <button
-                                    class="p-1 mb-2 mr-2 border border-transparent hover:border-blue-400 dark:border-gray-800 dark:hover:border-gray-400">
-                                    <div class="w-6 h-6 bg-green-300 "></div>
-                                </button>
-                                <button
-                                    class="p-1 mb-2 border border-transparent hover:border-blue-400 dark:border-gray-800 dark:hover:border-gray-400">
-                                    <div class="w-6 h-6 bg-red-200 "></div>
-                                </button>
-                            </div>
-                        </div>
-                        <div class="flex items-center mb-8">
-                            <h2 class="w-16 text-xl font-bold dark:text-gray-400">
+                            <h2 class="w-16 text-lg font-bold tracking-wider">
                                 Size:</h2>
-                            <div class="flex flex-wrap -mx-2 -mb-2">
+                            <div class="flex flex-wrap -mx-2 -mb-2 tracking-wider">
                                 <button
-                                    class="py-1 mb-2 mr-1 border w-11 hover:border-blue-400 dark:border-gray-400 hover:text-blue-600 dark:hover:border-gray-300 dark:text-gray-400">XL
+                                    class="py-1 mb-2 mr-1 border w-11 hover:border-blue-400 dark:border-gray-400 hover:text-blue-600 ">XL
                                 </button>
-                                <button
-                                    class="py-1 mb-2 mr-1 border w-11 hover:border-blue-400 hover:text-blue-600 dark:border-gray-400 dark:hover:border-gray-300 dark:text-gray-400">S
+                                <button class="py-1 mb-2 mr-1 border w-11 hover:border-blue-400 hover:text-blue-600 ">S
                                 </button>
-                                <button
-                                    class="py-1 mb-2 mr-1 border w-11 hover:border-blue-400 hover:text-blue-600 dark:border-gray-400 dark:hover:border-gray-300 dark:text-gray-400">M
+                                <button class="py-1 mb-2 mr-1 border w-11 hover:border-blue-400 hover:text-blue-600 ">M
                                 </button>
-                                <button
-                                    class="py-1 mb-2 mr-1 border w-11 hover:border-blue-400 hover:text-blue-600 dark:border-gray-400 dark:hover:border-gray-300 dark:text-gray-400">XS
+                                <button class="py-1 mb-2 mr-1 border w-11 hover:border-blue-400 hover:text-blue-600 ">XS
                                 </button>
                             </div>
                         </div>
-                        <div class="w-32 mb-8 ">
-                            <label for=""
-                                class="w-full text-xl font-semibold text-gray-700 dark:text-gray-400">Quantity</label>
-                            <div class="relative flex flex-row w-full h-10 mt-4 bg-transparent rounded-lg">
+                        <label for="" class="w-full text-lg tracking-wider font-semibold ">Quantity</label>
+                        <div class="grid md:grid-cols-3 grid-cols-1 items-center gap-10">
+                            <div class="">
+                                <div class="relative flex flex-row w-full h-12 mt-4 bg-transparent rounded-lg">
+                                    <button
+                                        class="w-20 h-full font-bold bg-gray-300 rounded-l outline-none cursor-pointer  hover:bg-gray-400">
+                                        <span class="m-auto text-2xl font-thin">-</span>
+                                    </button>
+                                    <input type="number"
+                                        class="flex items-center w-full font-semibold text-center text-black placeholder-black bg-gray-300 outline-nonefocus:outline-none text-md hover:text-black"
+                                        placeholder="1">
+                                    <button
+                                        class="w-20 h-full text-black bg-gray-300 rounded-r outline-none cursor-pointer  hover:bg-gray-400 font-semibold">
+                                        <span class="m-auto text-2xl font-thin">+</span>
+                                    </button>
+                                </div>
+                            </div>
+                            <div class=" col-span-2 h-12 mt-3">
                                 <button
-                                    class="w-20 h-full text-gray-600 bg-gray-300 rounded-l outline-none cursor-pointer dark:hover:bg-gray-700 dark:text-gray-400 hover:text-gray-700 dark:bg-gray-900 hover:bg-gray-400">
-                                    <span class="m-auto text-2xl font-thin">-</span>
-                                </button>
-                                <input type="number"
-                                    class="flex items-center w-full font-semibold text-center text-gray-700 placeholder-gray-700 bg-gray-300 outline-none dark:text-gray-400 dark:placeholder-gray-400 dark:bg-gray-900 focus:outline-none text-md hover:text-black"
-                                    placeholder="1">
-                                <button
-                                    class="w-20 h-full text-gray-600 bg-gray-300 rounded-r outline-none cursor-pointer dark:hover:bg-gray-700 dark:text-gray-400 dark:bg-gray-900 hover:text-gray-700 hover:bg-gray-400">
-                                    <span class="m-auto text-2xl font-thin">+</span>
-                                </button>
+                                    class="border border-black text-black w-full text-md tracking-widest py-3 rounded-lg transition duration-300 font-semibold hover:bg-black hover:text-white">Add
+                                    To Cart</button>
                             </div>
                         </div>
                         <div class="flex flex-wrap items-center -mx-4 ">
-                            <div class="w-full px-4 mb-4 lg:w-1/2 lg:mb-0">
+                            <div class="w-full px-4 mt-5">
                                 <button
-                                    class="flex items-center justify-center w-full p-4 text-blue-500 border border-blue-500 rounded-md dark:text-gray-200 dark:border-blue-600 hover:bg-blue-600 hover:border-blue-600 hover:text-gray-100 dark:bg-blue-600 dark:hover:bg-blue-700 dark:hover:border-blue-700 dark:hover:text-gray-300">
-                                    Add to Cart
+                                    class="flex items-center justify-center w-full py-3 rounded-lg font-semibold tracking-widest bg-black text-white border border-black transition duration-300 hover:bg-transparent hover:text-black">
+                                    Buy Now
                                 </button>
                             </div>
-                            <div class="w-full px-4 mb-4 lg:mb-0 lg:w-1/2">
-                                <button
-                                    class="flex items-center justify-center w-full p-4 text-blue-500 border border-blue-500 rounded-md dark:text-gray-200 dark:border-blue-600 hover:bg-blue-600 hover:border-blue-600 hover:text-gray-100 dark:bg-blue-600 dark:hover:bg-blue-700 dark:hover:border-blue-700 dark:hover:text-gray-300">
-                                    Add to wishlist
-                                </button>
+                        </div>
+                        <div class="md:flex tracking-widest gap-10 items-center my-5">
+                            <h1>
+                                <i class="fa-solid fa-right-left"></i><span class="ml-1">Compare</span>
+                            </h1>
+                            <h1>
+                                <i class="fa-regular fa-circle-question"></i> <span>Ask Question</span>
+                            </h1>
+                            <h1>
+                                <i class="fa-solid fa-share-nodes"></i> <span>Share</span>
+                            </h1>
+                        </div>
+                        <hr>
+                        <div class="mt-5">
+                            <h1 class="text-black tracking-wider text-md font-bold mb-2"> <i
+                                    class="fa-solid fa-truck-fast"></i>
+                                Estimated Delivery: <span class=" font-normal text-gray-600 ml-2">01 - 13 May,
+                                    2024</span>
+                            </h1>
+                            <h1 class="text-black tracking-wider text-md font-bold"> <i
+                                    class="fa-solid fa-truck-ramp-box"></i>
+                                Free
+                                Shipping & Returns: <span class=" font-normal text-gray-600 ml-2">On all orders over
+                                    $200.00</span>
+                            </h1>
+                        </div>
+                        <div class="bg-[#F7F7F7] py-5 mt-5 rounded-md flex flex-col items-center justify-center w-full">
+                            <div>
+                                <img src="https://minimog-4437.kxcdn.com/wp-content/themes/minimog/assets/woocommerce/product-trust-badge.png"
+                                    alt="">
+                            </div>
+                            <div>
+                                <h1 class="mt-2 tracking-wider">Guaranteed safe & secure checkout</h1>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
+    </section>
+    <section class="max-w-7xl mx-auto mt-20">
+        <div>
+            <div>
+                <h1 class="text-lg tracking-wider font-semibold">Product Details</h1>
+            </div>
+
         </div>
     </section>
     @include('layouts.footer')
